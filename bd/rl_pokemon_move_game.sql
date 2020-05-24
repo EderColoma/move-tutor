805f1281cd3bc770f352fe479a1b826a6df115a0
Eder Coloma <edermaceno@gmail.com>
2020-05-24 15:12:20 -0300

    Fixing include directories

--- bd/rl_pokemon_move_game.sql
+++ bd/rl_pokemon_move_game.sql
@@ -4,7 +4,8 @@ CREATE TABLE `rl_pokemon_move_game` ( `id_pokemon` int(11) NOT NULL DEFAULT '0',
 									  `level` int(11) DEFAULT NULL,
 									  `type` int(11) DEFAULT NULL,
 									  PRIMARY KEY (`id_pokemon`,`id_game`,`id_move`),
-									  FOREIGN KEY (`id_pokemon` ) REFERENCES `u802852207_mvtutor`.`pm_pokemon` (`ID_POKEMON` ),
-									  FOREIGN KEY (`id_move` ) REFERENCES `u802852207_mvtutor`.`pm_move` (`ID_MOVE` ),
-									  FOREIGN KEY (`id_game` ) REFERENCES `u802852207_mvtutor`.`pm_game` (`ID_GAME` ));
+									  FOREIGN KEY (`id_pokemon` ) REFERENCES `pm_pokemon` (`ID_POKEMON` ),
+									  FOREIGN KEY (`id_move` ) REFERENCES `pm_move` (`ID_MOVE` ),
+									  FOREIGN KEY (`id_game` ) REFERENCES `pm_game` (`ID_GAME` ));
+
 
--- db/DataAccess.php
+++ db/DataAccess.php
@@ -4,11 +4,9 @@ class DataAccess
 	
 	public function getDatabaseConnection()
 	{
-
 	    $link = mysqli_connect('us-cdbr-east-06.cleardb.net', 'heroku_24bc08400b830f5', '81e98594', 'b13e0a401ce94e');
 	    
 	    return $link;
-	    
 	}
 	
 	public function closeDatabaseConnection($link)
